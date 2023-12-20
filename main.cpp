9f6df5ab33ca8630d271166f37b5643deeba9e41
sofivasa <sofivasa@myuax.com>
2023-12-20 23:49:19 +0100

    commit3

--- main.cpp
+++ main.cpp
@@ -20,4 +20,45 @@ struct ConsoleBox
 };
 
 //Declaramos el objeto consoleBox
-ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
+ConsoleBox *consoleBox = new ConsoleBox; //  inicialización
+
+//Declaramos la función load_script
+
+void load_script(const char* filename, bool show_script = false)
+{
+    string script;
+    FILE* f = nullptr;
+    try
+    {
+        f = fopen(filename, "rb");
+        if (!f)
+        {
+            cerr << "error de apertura de " << filename << endl;
+            return;
+        }
+
+        int c;
+        char buf[4001];
+        while ((c = fread(buf, 1, 4000, f)) > 0)
+        {
+            buf[c] = 0;
+            script.append(buf);
+        }
+        fclose(f);
+        f = nullptr;
+
+        if (show_script)
+        {
+            cout << ColorConsole::fg_blue << ColorConsole::bg_white;
+            cout << script << endl;
+        }
+        consoleBox->new_text();
+        consoleBox->set_text(script);
+    }
+    catch (...)
+    {
+        cerr << "error durante la lectura del archivo" << endl;
+        if (f)
+            fclose(f);
+    }
+}
